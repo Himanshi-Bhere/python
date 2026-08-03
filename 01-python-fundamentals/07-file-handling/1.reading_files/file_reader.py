be2388676997be75cd1beae2c8098a334fdd391e
Himanshi <himanshibhere29@gmail.com>
2026-08-03 20:26:25 +0530

    file updated

--- 01-python-fundamentals/07-file-handling/1.reading_files/file_reader.py
+++ 01-python-fundamentals/07-file-handling/1.reading_files/file_reader.py
@@ -6,3 +6,12 @@ print(file.read(7)) # read first 7 characters of the file
 print(file.read()) # read the rest of the file
 
 file.close() #close the file after reading 
+
+file = open("sample.txt", "r") # open file in read mode
+
+print("complete the file content:")
+print("--------------------------------")
+print(file.read(7)) # read first 7 characters of the file
+print(file.read()) # read the rest of the file
+
+file.close() #close the file after reading 
